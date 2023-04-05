94a3d70f742265052ddad4313b5ed9a8c7fb80fe
Nazar <nazar.mazur11@otlook.com>
2023-04-05 15:45:18 +0300

    Main + Booking skeleton

---
+++ Service/src/main/kotlin/com/boxes/Service/MainController.kt
@@ -0,0 +1,14 @@
+package com.boxes.Service
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/")
+class TokenController {
+    @GetMapping("main")
+    fun returnToken() : String {
+     return "HELLO"
+    }
+}
