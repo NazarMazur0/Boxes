9846633b8457f6832042970cc217a5eaa5dfcce4
Nazar <104011247+NazarMazur0@users.noreply.github.com>
2023-05-24 16:29:12 +0300

    Delete Service directory

--- Service/src/main/kotlin/com/boxes/Service/models/Booking.kt
+++
@@ -1,7 +0,0 @@
-package com.boxes.Service.models
-
-class Booking(val name: String,val surname: String,val email: String,val phone:String,val period:Int,val size:Int,val check:Boolean,val wishes:String) {
-    override fun toString(): String {
-        return "Booking(name='$name', surname='$surname', email='$email', phone='$phone', period=$period, size=$size, check=$check, wishes='$wishes')"
-    }
-}
